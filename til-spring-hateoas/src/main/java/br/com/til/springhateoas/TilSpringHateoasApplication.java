c13f4f4fda449d273234f25d50b73671d389681c
CarlosGeorge <carlosgeorge248@gmail.com>
2021-09-05 10:00:32 -0300

    First Commit

---
+++ til-spring-hateoas/src/main/java/br/com/til/springhateoas/TilSpringHateoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.til.springhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilSpringHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilSpringHateoasApplication.class, args);
+	}
+
+}
---
+++ til-spring-hateoas/src/main/java/br/com/til/springhateoas/model/Product.java
@@ -0,0 +1,74 @@
+package br.com.til.springhateoas.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.hateoas.RepresentationModel;
+
+@Entity
+@Table(name="PRODUCT")
+public class Product extends RepresentationModel<Product> implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="PRICE")
+	private BigDecimal price;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+
+}
